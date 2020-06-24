70f06633d7492ffb5de867b26a169a0e0b7705bc
pimpale <gpimpale29@gmail.com>
2020-06-23 20:57:43 -0700

    finished nice round card thing

---
+++ src/components/Layout.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { ThreeDotsVertical } from 'react-bootstrap-icons'
+
+// Bootstrap CSS & JS
+import '../styles/style.scss';
+import 'bootstrap/dist/js/bootstrap';
+import 'popper.js/dist/popper';
+
+function Header() {
+  return (
+    <header className="header">
+      <nav className="navbar navbar-expand-lg py-3 fixed-top">
+        <div className="container">
+          <a className="navbar-brand font-weight-bold" href="index.html"></a>
+          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+            <ThreeDotsVertical className="text-light"/>
+          </button>
+          <div className="collapse navbar-collapse"
+            id="navbarSupportedContent">
+            <div className="navbar-nav ml-auto">
+              <a className="nav-item nav-link font-weight-bold" href="about.html">About</a>
+              <a className="nav-item nav-link font-weight-bold" href="faq.html">FAQ</a>
+              <a className="nav-item nav-link font-weight-bold" href="https://github.com/pimpale/pimpale.github.io">Source</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="container-fluid py-1"><br />
+      <p className="text-light">By Govind Pimpale</p>
+    </footer>
+  )
+}
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <div className="bg-dark text-light">
+        <Header />
+        <div className="content"> {this.props.children} </div>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default Layout;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,70 +2,141 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 
+type IntroCardProps = {
+  title: string,
+  rotation: number,
+  link: string,
+}
+
+const IntroCard: React.FunctionComponent<IntroCardProps> = props => {
+  const circleEdge = {
+    transform: `rotate(${props.rotation}deg) translate(20vw) rotate(${-props.rotation}deg) translate(-50%, -50%)`,
+    transformOrigin: "top left",
+    display: "block",
+    position: "absolute" as const,
+    top: "50%",
+    left: "50%",
+  };
+  return (
+    <div className="card text-light bg-dark" style={circleEdge}>
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <div className="card-text">{props.children}</div>
+        <a href={props.link} className="stretched-link d-inline" />
+      </div>
+    </div>
+  );
+}
+
+
+const introStyle = {
+  alignItems: "center",
+  backgroundAttachment: "fixed",
+  backgroundPosition: "center center",
+  backgroundRepeat: "noRepeat",
+  backgroundSize: "cover",
+  display: "flex",
+  minHeight: "100vh",
+  width: "90vw",
+  paddingTop: "5vh",
+  paddingBottom: "5vh",
+}
+
+
+const circleCenter = {
+  transformOrigin: "center",
+  display: "block",
+  position: "absolute" as const,
+  top: "50%",
+  left: "50%",
+}
+
 function Home() {
   return (
     <Layout>
-      <section className="maincontent">
-        <div className="container">
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
-          <p>Home</p>
+      <section id="intro">
+        <div style={introStyle}>
+          <div>
+            <IntroCard rotation={-30} title="About" link="#about">
+              About this site
+            </IntroCard>
+            <IntroCard rotation={0} title="About" link="#about">
+              About this site
+            </IntroCard>
+            <IntroCard rotation={30} title="About" link="#about">
+              About this site
+            </IntroCard>
+            <IntroCard rotation={-30 + 180} title="About" link="#about">
+              About this site
+            </IntroCard>
+            <IntroCard rotation={0 + 180} title="About" link="#about">
+              About this site
+            </IntroCard>
+            <IntroCard rotation={30 + 180} title="Source" link="https://github.com/pimpale/pimpale.github.io">
+              View source code for this website (Public Domain)
+            </IntroCard>
+          </div>
         </div>
+      </section>
+      <section id="content" className="container">
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
       </section>
     </Layout>
   )
