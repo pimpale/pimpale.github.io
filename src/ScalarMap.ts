4df270edfcbe8e7cf43f461b3c7d1c52b21580c4
pimpale <gpimpale29@gmail.com>
2020-07-08 15:43:07 -0400

    stuff

---
+++ src/ScalarMap.ts
@@ -0,0 +1,33 @@
+import assert from 'assert';
+
+class ScalarMap {
+  private arr:Uint16Array;
+  private xsize:number;
+  private ysize:number;
+
+  constructor(xsize:number, ysize:number) {
+    this.arr = new Uint16Array(xsize*ysize);
+    this.xsize = xsize;
+    this.ysize = ysize;
+  }
+
+  getv(x:number, y:number) {
+    assert(x < this.xsize);
+    assert(y < this.ysize);
+    return this.arr[this.xsize*y + x] / 0xFFFF;
+  }
+
+  setv(x:number, y:number, v:number) {
+    assert(x < this.xsize);
+    assert(y < this.ysize);
+    assert(v >= 0 && v <= 1.0);
+    this.arr[this.xsize*y + x] = v * 0xFFFF;
+  }
+
+  dims() {
+    return { width: this.xsize, height: this.ysize };
+  }
+
+}
+
+export default ScalarMap;
---
+++ src/VectorMap.ts
@@ -0,0 +1,35 @@
+import assert from 'assert';
+
+class VectorMap {
+  private arr: Float32Array;
+  private xsize: number;
+  private ysize: number;
+
+  constructor(xsize: number, ysize: number) {
+    this.arr = new Float32Array(xsize * ysize * 2);
+    this.xsize = xsize;
+    this.ysize = ysize;
+  }
+
+  getv(x: number, y: number) {
+    assert(x < this.xsize);
+    assert(y < this.ysize);
+    return [this.arr[(this.xsize * y + x) * 2 + 0],
+    this.arr[(this.xsize * y + x) * 2 + 1]];
+  }
+
+  setv(x: number, y: number, v: number[]) {
+    assert(x < this.xsize);
+    assert(y < this.ysize);
+    let [vx, vy] = v;
+    this.arr[(this.xsize * y + x) * 2 + 0] = vx;
+    this.arr[(this.xsize * y + x) * 2 + 0] = vy;
+  }
+
+  dims() {
+    return { width: this.xsize, height: this.ysize };
+  }
+
+}
+
+export default VectorMap;
