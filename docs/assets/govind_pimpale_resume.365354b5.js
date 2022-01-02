64b71b6a38447c591f1c312f1d530423cd41b401
gpi <gpimpale29@gmail.com>
2022-01-01 23:57:14 -0800

    stuff

--- docs/assets/govind_pimpale_resume.365354b5.js
+++
@@ -1,1 +0,0 @@
-var e="/assets/govind_pimpale_resume.8d3d8967.pdf";export{e as R};
--- src/utils/load.ts
+++ src/utils/load.ts
@@ -1,23 +1,6 @@
-import {Result, staticUrl} from '@innexgo/frontend-common';
-
-export const LoadErrorCodes = [
-  "NETWORK",
-] as const;
-
-// Creates a union type
-export type LoadErrorCode = typeof LoadErrorCodes[number];
-
-
 // this function may throw
 export async function fetchText(url:string): Promise<string> {
-  let data = await fetch(`${staticUrl()}/${url}`);
+  let data = await fetch(url);
   return await data.text()
 }
 
-async function fetchTextOrNetworkError(url: string): Promise<Result<string, LoadErrorCode>> {
-  try {
-    return { Ok: await fetchText(url) };
-  } catch (_) {
-    return { Err: "NETWORK" };
-  }
-}
