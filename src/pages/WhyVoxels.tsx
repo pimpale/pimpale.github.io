72097f4a93f892c9d9bffc3530f00cdc0e9c8bbd
gpi <gpimpale29@gmail.com>
2022-06-22 16:41:59 -0700

    add more posts

---
+++ src/pages/WhyVoxels.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import Section from '../components/Section';
+import HrefLink from '../components/HrefLink';
+import ArticleLayout from '../components/ArticleLayout';
+
+import AsideCard from '../components/AsideCard';
+
+import WebGL2GravityDemo from '../components/WebGL2GravityDemo';
+import LennardJonesDemo from '../components/LennardJonesDemo';
+
+const WhyVoxelsPage  = () => <ArticleLayout>{
+  ({ Citation, CitationBank }) => <>
+    <Section name="Spacewar" id="spacewar">
+      <p>
+        <HrefLink href="https://github.com/pimpale/spacewar" />
+      </p>
+      <p>
+        <HrefLink href="https://github.com/pimpale/GravitySimulator" />
+      </p>
+      <AsideCard title="Canvas Setup" id="canvas-setup-demo">
+        <WebGL2GravityDemo
+          className="mx-auto"
+          style={{maxWidth: "50em"}}
+          size={512}
+        />
+      </AsideCard>
+
+      <AsideCard title="Canvas Setup" id="canvas-setup-demo">
+        <LennardJonesDemo
+          className="mx-auto"
+          style={{maxWidth: "50em"}}
+          size={512}
+        />
+      </AsideCard>
+
+    </Section>
+  </>
+}</ArticleLayout>
+
+import { createRoot } from 'react-dom/client';
+
+// Bootstrap CSS & JS
+import '../styles/style.scss';
+import 'bootstrap/dist/js/bootstrap';
+
+const root = createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <WhyVoxelsPage />
+  </React.StrictMode>,
+);
