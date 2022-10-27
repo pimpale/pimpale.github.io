b1c183790e3f865aba074fbca0e036c3ba0ba7f3
gpi <gpimpale29@gmail.com>
2022-10-26 23:58:17 -0700

    updates

--- docs/assets/achernar.963791d6.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill.c7c6310f.js";import{L as o,S as e}from"./Section.59c5635d.js";import{R as r,j as t,a as i}from"./bootstrap.0afc7782.js";function n(){return t(o,{children:t(e,{id:"motivation",name:"Motivation",children:"Todo..."})})}r.render(t(i.StrictMode,{children:t(n,{})}),document.getElementById("root"));
---
+++ docs/assets/achernar.97590dfb.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill.c7c6310f.js";import{L as o,S as e}from"./Section.43251198.js";import{R as r,j as t,a as i}from"./bootstrap.0afc7782.js";function n(){return t(o,{children:t(e,{id:"motivation",name:"Motivation",children:"Todo..."})})}r.render(t(i.StrictMode,{children:t(n,{})}),document.getElementById("root"));
