a8587fd454c140eb4fef1d7ae338e113464feff0
gpi <gpimpale29@gmail.com>
2024-11-29 15:54:16 +0000

    update to vite 6

--- docs/assets/client-Bw8Scv2q.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./index-BzpxhVhl.js";var o={},r,t=a;r=o.createRoot=t.createRoot,o.hydrateRoot=t.hydrateRoot;export{o as a,r as c};
---
+++ docs/assets/index-ClGTLc6W.js
@@ -0,0 +1,1 @@
+import{b as r,g as t}from"./index-BCoGhUYq.js";var o=r();const e=t(o);export{e as R};
